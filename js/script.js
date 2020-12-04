84c42de8df91addec5983779d6a8196bbf832932
kt-std <kt.std2020@gmail.com>
2020-12-04 23:07:15 +0200

    Add tab background animation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
+const ASSETS_PATH = "assets/";
+
 document.querySelector("#menu-btn").addEventListener('click', function display(e){
 	document.querySelector("#menu").classList.toggle("visible");
 });
 
 document.querySelector("#menu").addEventListener('click', function display(e){
-	console.log(e);
-	e.target.classList.toggle("close");
+	let tabText = e.target.textContent.toLowerCase();
+	let tabImage = document.getElementById("tabImage");
+	updateBackground(tabImage, ASSETS_PATH, tabText);
 });
+
+
+let updateBackground = (element, path, id) => {
+	element.style.backgroundImage = window.innerWidth < 720 ? `url('${path + "_" + id}.jpg')` : `url('${path + id}.jpg')`;
+	element.animate([{opacity: .5},{opacity: 1}], {duration: 500});
+}
